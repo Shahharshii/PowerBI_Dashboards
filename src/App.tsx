546d0861108dc9dc05c5c732a6c7bc7e0c9f16ff
Shahharshii <shahharshi694@gmail.com>
2024-09-23 00:51:31 +0530

    final changes

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import { slides } from "./data";
+import "yet-another-react-lightbox/styles.css";
+import {
+  Captions,
+  Download,
+  Thumbnails,
+} from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Images from "./Images";
+
+function App() {
+  // const [open, setOpen] = useState<boolean>(false);
+  const [index, setIndex] = useState<number>(-1);
+
+  return (
+    <>
+      {/* <button onClick={() => setOpen(true)}>Open Lightbox</button> */}
+     <h1 className="header">Dashboard</h1>
+      <Images
+        data={slides}
+        onClick={(currentIndex) => setIndex(currentIndex)}
+      />
+
+      <Lightbox
+        plugins={[Captions, Download, Thumbnails]}
+        captions={{
+          showToggle: true,
+          descriptionTextAlign: "end",
+        }}
+        // open={open}
+        // close={() => setOpen(false)}
+
+        index={index}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        slides={slides}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,52 @@
+export const slides = [
+  {
+    src: '/dash10.jpg',
+    title: 'Dashboard1',
+    description: 'Used basic card and gauge for visualization.',
+  },
+  {
+    src: '/dash9.jpg',
+    title: 'Assignment 1',
+    description: '',
+  },
+  {
+    src: '/dash8.jpg',
+    title: 'Another way of import',
+    description: 'import data from the imdb movies site, made the dashboard on the live data',
+  },
+  {
+    src: '/dash4.jpg',
+    title: 'Types of visualization',
+    description: '',
+  },
+  {
+    src: '/dash5.jpg',
+    title: 'Types of vizualization',
+    description: '',
+  },
+  {
+    src: '/dash6.jpg',
+    title: 'Types of vizualization',
+    description: '',
+  },
+  {
+    src: '/dash7.jpg',
+    title: 'Types of vizualization',
+    description: '',
+  },
+  {
+    src: '/dash3.jpg',
+    title: 'Assignment 4',
+    description: '',
+  },
+  {
+    src: '/dash2.jpg',
+    title: 'Dashboard 2',
+    description: '',
+  },
+  {
+    src: '/dash1.jpg',
+    title: 'Data regression',
+    description: '',
+  },
+]
